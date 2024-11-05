8df6acda4f000e3c5cecbddc1a3247a83f758cf3
LuizBecker01 <luizhenriquebecker000@gmail.com>
2024-11-05 18:02:36 -0300

    05/11/2024
    ...

---
+++ app/__init__.py
@@ -0,0 +1,16 @@
+from flask import Flask
+from app.connection import engine
+from app.models import Base 
+
+# Criação do objeto Flask
+app = Flask(__name__)
+
+# Configurações de banco de dados
+app.config['SQLALCHEMY_DATABASE_URI'] = "postgresql://postgres:2404@localhost:5432/system"
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+
+# Associando o objeto 'app' aos modelos e engine
+Base.metadata.bind = engine
+
+# Defina suas rotas aqui
+from app import routes
--- run.py
+++ run.py
@@ -1,5 +1,9 @@
 from app.connection import engine,Base
 from app import models
+from app import app
 
 #Cria as ta belas do banco
 Base.metadata.create_all(bind=engine)
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=8001, debug=True)

fb7660a5b5a7bc0244448d736ca764eddf838120
LuizBecker01 <luizhenriquebecker000@gmail.com>
2024-11-12 14:46:44 -0300

    12/11/24
    ...

--- run.py
+++ run.py
@@ -5,4 +5,4 @@ from app import app
 if __name__ == "__main__":
     with app.app_context():
         Base.metadata.create_all(bind=engine)   # Cria as tabelas do banco de dados
-    app.run(host="0.0.0.0", port=6000, debug=True)
+    app.run(host="0.0.0.0", port=8001, debug=True)
